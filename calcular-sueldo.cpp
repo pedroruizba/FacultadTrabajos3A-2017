ac5764ab3d2a7a7e94f755c0f9721a3651a71f57
LAPTOP-L5SF3LP1\pedro <pedroantonioruiz.b@gmail.com>
2017-08-15 22:48:28 -0500

    Sin necesidad de usar otras variables, calcula el nuevo sueldo de acuerdo a la siguiente tabla rango aumento 0 - 1000 18% 1001 - 1100 15% 1101 - 1200 12% 1201 - 1300 10% mas de 1300 8%

---
+++ calcular-sueldo.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float sueldo, porcentaje;
+	cout<<"Ingresa tu sueldo actual: ";
+	cin>>sueldo;
+	
+	if (sueldo > 1300){
+		cout<<"8%";
+		sueldo=(sueldo*8)/100+sueldo;
+	}
+	else{
+		if ((sueldo <= 1300)&&(sueldo >= 1201)){
+		cout<<"10%";
+		sueldo=(sueldo*10)/100+sueldo;	
+	}
+	else{
+		if ((sueldo <= 1200)&&(sueldo >= 1101)){
+		cout<<"12%";
+		sueldo=(sueldo*12)/100+sueldo;
+	}
+	else
+	{
+		if ((sueldo <= 1100)&&(sueldo >= 1001)){
+		cout<<"15%";
+		sueldo=(sueldo*15)/100+sueldo;
+	}
+	else{
+		if ((sueldo <= 1000)&&(sueldo >= 0)){
+		cout<<"18%";
+		sueldo=(sueldo*18)/100+sueldo;
+	}
+	}
+	}
+	}
+	}
+
+	cout<<"\nTu nuevo sueldo es $"<<sueldo;
+	return 0;
+}
