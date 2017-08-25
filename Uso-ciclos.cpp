d15109fc44e1c167a8c92c028422d475550f0804
LAPTOP-L5SF3LP1\pedro <pedroantonioruiz.b@gmail.com>
2017-08-25 10:11:54 -0500

    Tarea uso de ciclos

---
+++ Uso-ciclos.cpp
@@ -0,0 +1,97 @@
+# include <stdio.h>
+# include <conio.h>
+# include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+    int opc = 0,i,factorial,numero, a=0,b,numeroprimo,calificacion,suma=0, contador = 0;
+	float promedio;
+
+
+ while(opc>=0)
+ {
+    system("cls");
+
+
+   cout<<"Que desea hacer? ";
+   cout<<endl<<"========================";
+   cout<<endl<<"= Numero primo    1    =";
+   cout<<endl<<"= Factorial       2    =";
+   cout<<endl<<"= Calificaciones  3    =";
+   cout<<endl<<"= Salir           0    =";
+   cout<<endl<<"========================";
+   cout<<endl<<"====> ";
+   cin>>opc;
+
+
+    switch(opc)
+    {
+        case 1:
+		 	cout<<"Ingrese numero"<<endl;
+         	cin>>numeroprimo;
+         	for(b=1;b<(numeroprimo+1);b++)
+		 	{
+         		if(numeroprimo%b==0)
+				{
+            		a++;
+        		}
+         	}
+        	if(a!=2)
+			{
+        		cout<<"No es Primo";
+        	}
+			else
+			{
+        		cout<<"Si es Primo";
+        	}
+        	a=0;
+        	b=0;
+        	numeroprimo=0;
+        break;
+        case 2:
+        	cout<<"Sacar el factorial"<<endl;
+         	cout<<"Ingresa un numero: ";
+         	cin>>numero;
+         		for(i=1;i<=numero;i++)
+				{
+     				factorial*=i;
+     				cout<<i<<"x";
+				}
+			cout<<"\nEl factorial de numero es : "<<factorial;
+        break;
+        case 3:
+        	cout<<"Ingresa las calificaciones y coloca un 0 o menor a 0 para terminar y calcular promedio\n";
+			while(calificacion > 0)
+  			{
+    			cout<<"Ingrese la calificacion: "<<endl;
+    			cin>>calificacion;
+    			if(calificacion != 0)
+    			{
+      				calificacion+=calificacion;
+      				contador+=1;
+    			}
+    			else
+    			{
+    				contador-=1;
+    				promedio=calificacion/contador;
+      				cout<<"Promedio: "<<promedio<<endl;
+    			}
+
+  			}			
+        break;
+
+        case 0:
+        return 0;
+        break;
+
+
+        default:
+            cout<<"Error! ";
+   }
+	getch();
+ }
+
+}
